7d45d9f8f7b5a488e119e63379a1758e9e3adb7b
enjiaja <njiaja222@gmail.com>
2025-08-17 01:17:48 +0700

    Create script.js

---
+++ script.js
@@ -0,0 +1,7 @@
+// Hamburger Menu
+const hamburger = document.getElementById("hamburger");
+const nav = document.querySelector("nav");
+
+hamburger.addEventListener("click", () => {
+  nav.classList.toggle("active");
+});
